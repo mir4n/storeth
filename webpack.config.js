63e056b54ec6a38d7b8de639e8aa0ec9ea748804
Miran saman <miran.saman.04@gmail.com>
2022-12-18 22:52:07 +0300

    fixed webpack.config

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,20 @@
 const path = require("path");
 
 module.exports = {
-  output: {
-    filename: "storeth.bundle.js",
-  },
-  module: {
-    rules: [{ test: /\.txt$/, use: "raw-loader" }],
-  },
-};
+    entry: "./lib/index.js",
+    output: {
+        path: path.join(__dirname, 'public'),
+        filename: 'bundle.js'
+    },
+    module:{
+        rules:[{
+            loader: 'babel-loader',
+            test: /\.(js|jsx)$/,
+            exclude: /node_modules/
+        }]
+    },
+    devtool: 'cheap-module-eval-source-map',
+    devServer: {
+        contentBase: path.join(__dirname, 'public')
+    }
+}

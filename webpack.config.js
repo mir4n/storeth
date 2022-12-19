32a1a6f6e438a40cdd6ca22e29e1036c319c9919
Miran saman <miran.saman.04@gmail.com>
2022-12-19 09:14:47 +0300

    fixed webpack.config

--- webpack.config.js
+++ webpack.config.js
@@ -1,20 +1,54 @@
 const path = require("path");
-
+ 
 module.exports = {
-    entry: "./lib/index.js",
-    output: {
-        path: path.join(__dirname, 'public'),
-        filename: 'bundle.js'
-    },
-    module:{
-        rules:[{
-            loader: 'babel-loader',
-            test: /\.(js|jsx)$/,
-            exclude: /node_modules/
-        }]
-    },
-    devtool: 'cheap-module-eval-source-map',
-    devServer: {
-        contentBase: path.join(__dirname, 'public')
-    }
-}
+ entry: path.join(_dirname, "lib/index.js"),
+ module: {
+   rules: [
+     {
+       test: /\\.(js|jsx)?$/,
+       include: path.resolve(_dirname, "lib"),
+       exclude: /node\_modules/,
+       use: [
+         {
+           loader: "babel-loader",
+           options: {
+             cacheDirectory: true,
+             presets: ["module:metro-react-native-babel-preset"],
+             plugins: ["react-native-web"],
+           },
+         },
+       ],
+     },
+     {
+       test: /\\.(gif|jpe?g|png|svg)$/,
+       use: {
+         loader: "url-loader",
+         options: {
+           name: "\[name\].\[ext\]",
+           esModule: false,
+         },
+       },
+     },
+   ],
+ },
+ resolve: {
+   extensions: [".ts", ".tsx", ".js", ".jsx"],
+   alias: {
+     "react-native$": "react-native-web",
+   },
+ },
+ externals: {
+   react: {
+     commonjs: "react",
+     commonjs2: "react",
+     amd: "React",
+     root: "React",
+   },
+   "react-dom": {
+     commonjs: "react-dom",
+     commonjs2: "react-dom",
+     amd: "ReactDOM",
+     root: "ReactDOM",
+   },
+ },
+};
